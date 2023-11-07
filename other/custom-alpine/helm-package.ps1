ab83d7e37121d27b3a2351601bee03dfcf0534b0
Kaleb Pederson <kpederson@healthequity.com>
2023-11-07 10:06:00 -0700

    Make helm charts use image digest.

--- other/custom-alpine/helm-package.ps1
+++ other/custom-alpine/helm-package.ps1
@@ -3,4 +3,10 @@ $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
 
 $VERSION=(./get-version.ps1)
+$IMAGEDIGEST=(./get-latestdigest.ps1)
+
+# update values file to include the image digest
+(Get-Content charts\values.yaml) -replace 'digest: .*', "digest: `"$IMAGEDIGEST`"" | Set-Content charts\values.yaml
+
+# package the helm chart
 helm package charts --app-version "0.$VERSION.0" --version "0.$VERSION.0"
