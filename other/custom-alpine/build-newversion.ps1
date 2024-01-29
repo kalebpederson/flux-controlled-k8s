b1f5091b6621e98b1f7c8118bf5df7160cb2125a
Kaleb Pederson <kpederson@healthequity.com>
2024-01-29 16:46:31 -0700

    Add OPA gatekeeper.

--- other/custom-alpine/build-newversion.ps1
+++ other/custom-alpine/build-newversion.ps1
@@ -4,5 +4,6 @@ $PSNativeCommandUseErrorActionPreference = $true
 
 $VERSION=((./get-version.ps1)+1)
 $REPOPATH=(./get-repopath.ps1)
+write-host docker build . --build-arg CACHEBUST=$(Get-Random) --tag custom-alpine:latest --tag "$($REPOPATH):0.$VERSION.0"
 docker build . --build-arg CACHEBUST=$(Get-Random) --tag custom-alpine:latest --tag "$($REPOPATH):0.$VERSION.0"
 echo $VERSION > .version
